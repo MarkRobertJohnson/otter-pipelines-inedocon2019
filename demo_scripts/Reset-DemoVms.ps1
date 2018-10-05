434184c4cf715f52c0baf62a2d4af195802e0d9d
Mark Johnson <mjohnson@axian.com>
2018-10-05 12:40:36 -0700

    Squashed all commits on branch dev

---
+++ demo_scripts/Reset-DemoVms.ps1
@@ -0,0 +1,8 @@
+﻿param([string]$VmxPath, 
+[string]$SnapShotName)
+
+Write-Host -ForegroundColor green "Reverting '$VmxPath' to Snapshot '$SnapShotName' ..."
+#restore VM snapshot
+& "C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" -T ws revertToSnapshot "$VmxPath" "$SnapShotName"
+Write-Host -ForegroundColor green "Starting VM '$VmxPath' ..."
+& "C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" -T ws start "$VmxPath" 
