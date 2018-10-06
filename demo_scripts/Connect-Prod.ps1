3f7901c24a8da42c9ed609391dd6e89a7b805b16
Mark Johnson <mjohnson@axian.com>
2018-10-06 16:58:30 -0700

    Updated VM snapshot

--- demo_scripts/Connect-Prod.ps1
+++ demo_scripts/Connect-Prod.ps1
@@ -3,4 +3,4 @@ $password = ("skor4me!" | ConvertTo-SecureString -asPlainText -Force)
 )
 $credential = New-Object System.Management.Automation.PSCredential ($username,$password)
 
-enter-pssession -ComputerName s16-dev -credential $credential
+enter-pssession -ComputerName s16-prod -credential $credential
