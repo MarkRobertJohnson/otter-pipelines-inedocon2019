7b3a12c027047b41c520d522908b359263124c54
Mark Johnson <mjohnson@axian.com>
2018-10-06 21:03:26 -0700

    Squashed all commits on branch dev

---
+++ demo_scripts/Connect-Dev.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    $username = "mark johnson",
+    $password = ("skor4me!" | ConvertTo-SecureString -asPlainText -Force),
+    $computerName = 's16-dev'
+)
+$credential = New-Object System.Management.Automation.PSCredential ($username,$password)
+$session = New-PSSession -ComputerName $computerName -Credential $credential 
+Invoke-Command -Session $session -ScriptBlock { 
+    if(Test-Path "c:\ProgramData\Chocolatey\bin\RefreshEnv.cmd") { 
+        . "c:\ProgramData\Chocolatey\bin\RefreshEnv.cmd"
+    }
+    
+}
+[PSCustomObject] @{ 
+        CommandLine= '. "c:\ProgramData\Chocolatey\bin\RefreshEnv.cmd"'; 
+        ExecutionStatus = "Completed"; 
+        StartExecutionTime=(Get-Date).ToString(); 
+        EndExecutionTime=(Get-Date).ToString() } | Add-History 
+enter-pssession -Session $session 
