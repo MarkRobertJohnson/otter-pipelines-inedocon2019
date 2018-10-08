d91954a5506e5256d4ab442a112f4e5d85284518
Mark Johnson <mjohnson@axian.com>
2018-10-07 22:47:43 -0700

    Squashed all commits on branch master

---
+++ demo_scripts/Prepare-OtterServerForDemo.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host -ForegroundColor Green "Running server checker ..."
+Invoke-WebRequest -Uri http://s16-dev:8626/0x44/Otter.WebApplication/Inedo.Otter.WebApplication.Pages.Administration.ServiceMonitorPage/RunExecuter?name=Server%20Checker -Method POST -Verbose 
+
+Write-Host -ForegroundColor Green "Running RoutineConfigurationRunner ..."
+Invoke-WebRequest -Uri http://s16-dev:8626/0x44/Otter.WebApplication/Inedo.Otter.WebApplication.Pages.Administration.ServiceMonitorPage/RunExecuter?name=RoutineConfigurationRunner -Method POST -Verbose
